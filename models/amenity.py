0cc57630e9c26cfdf332061feccc0ff1bff8d5f4
tony <tony@computer.com>
2022-10-30 22:23:50 +0100

    added anenity.py
    defines the class Amenity

---
+++ models/amenity.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# amenity.py
+
+""" Defines the Amenity class """
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """ Subclass of BaseModel class
+
+        Args:
+            name(str): name of the amenity
+
+    """
+    name = ''
+    def __init__(self, *args, **kwargs):
+        """ Initialises the Amenity class
+
+            Args:
+                args(tuple): Not used
+                kwargs(dictionary): Dictionary representation of instance
+
+        """
+        super().__init__(**kwargs)
